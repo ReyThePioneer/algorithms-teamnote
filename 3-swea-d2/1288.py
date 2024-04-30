fed6de11b388e9dd1a21e24b0c0aee36bdae231c
yh.hong <ghddpgml@gmail.com>
2024-04-30 13:18:39 +0900

    swea d2 #1288

--- 3-swea-d2/1288.py
+++ 3-swea-d2/1288.py
@@ -1,19 +1,19 @@
 T = int(input())
-num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for i in range(T):
-  N = int(input())
+  n = int(input())
+  s = set()
   k = 1
-  num_list = []
 
   while (True):
-    k += 1
-    str_N = list(str(N * k))
-    num_list.extend(str_N)
+    for j in list(str(n)):
+      s.add(j)
     
-    a = list(map(int, list(set(num_list))))
-    a.sort()
-
-    if a == num:
-      print(f"#{T + 1} {N * k}")
+    if len(s) == 10:
       break
+
+    n //= k
+    k += 1
+    n *= k
+  
+  print(f"#{i + 1} {n}")
