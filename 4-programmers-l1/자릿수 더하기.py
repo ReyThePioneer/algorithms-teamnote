b4c473edc62efdbfe84395a1b0c4828dd4e19322
yh.hong <ghddpgml@gmail.com>
2024-05-02 23:20:52 +0900

    EDIT readme

--- 4-programmers-l1/자릿수 더하기.py
+++ 4-programmers-l1/자릿수 더하기.py
@@ -1,9 +1,7 @@
-def solution(num):
+def solution(n):
     sum = 0
 
-    a = list(str(num))
-
-    for i in range(len(a)):
-        sum += int(a[i])
-
-solution(123)
+    for i in str(n):
+        sum += int(i)
+    
+    return sum
