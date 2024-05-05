314c454903fc09756772fdc916962b54baac2564
yh.hong <ghddpgml@gmail.com>
2024-05-05 17:10:15 +0900

    swea D2 #1959

--- 3-swea-d2/1959.py
+++ 3-swea-d2/1959.py
@@ -1,33 +1,20 @@
-T = int(input())
+t = int(input())
 
-for i in range(T):
-  N, M = map(int, input().split())
-  A = list(map(int, input().split()))
-  B = list(map(int, input().split()))
+for i in range(t):
+  n, m = map(int, input().split())
+  a = list(map(int, input().split()))
+  b = list(map(int, input().split()))
   array = []
-  sum = 0
 
-  if (N < M):
-    for j in range(M - N + 1):
-      cnt = 0
-      for k in range(i, i + N):
-        sum += A[cnt] * B[k]
-        cnt += 1
-      array.append(sum)
-      sum = 0
-  
-  elif (N > M):
-    for l in range(N - M + 1):
-      cnt = 0
-      for m in range(l, l + M):
-        sum += A[m] * B[cnt]
-        cnt += 1
-      array.append(sum)
-      sum = 0
-  
-  else:
-    for n in range(N):
-      sum += A[n] * B[n]
+  if (n > m):
+    m, n = n, m
+    a, b = b, a
+
+  while (n <= len(b)):
+    sum = 0
+    for j in range(n):
+      sum += a[j] * b[j]
     array.append(sum)
+    b.remove(b[0])
 
   print(f"#{i + 1} {max(array)}")
