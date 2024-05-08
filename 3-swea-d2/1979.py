60846d4b1e167d43bef50259cd43465bcf953379
yh.hong <ghddpgml@gmail.com>
2024-05-08 21:21:12 +0900

    swea D2 #1979(ongoing)

---
+++ 3-swea-d2/1979.py
@@ -0,0 +1,16 @@
+#1979 어디에 단어가 들어갈 수 있을까
+
+T = int(input())
+
+for t in range(T):
+  N, K = map(int, input().split())
+  arr = []
+  ans = 0
+
+  for n in range(N):
+    arr.append(list(map(int, input().split())))
+
+    for m in range(N):
+      pass
+
+  # print(f"#{t + 1} {ans}")
