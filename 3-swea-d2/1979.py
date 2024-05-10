52b051227c6f3887611de08fc5600f61d1071797
yh.hong <ghddpgml@gmail.com>
2024-05-10 23:28:02 +0900

    swea D2 #1979(ongoing)

--- 3-swea-d2/1979.py
+++ 3-swea-d2/1979.py
@@ -5,12 +5,16 @@ T = int(input())
 for t in range(T):
   N, K = map(int, input().split())
   arr = []
-  ans = 0
 
-  for n in range(N):
-    arr.append(list(map(int, input().split())))
+  for i in range(N):
+    arr[i] = list(map(int, input().split()))
+    sum_arr = []
+    sum = 0
+    print(arr)
 
-    for m in range(N):
-      pass
+    for j in range(N):
+      while (arr[i][j] == 1):
+        sum += 1
+    sum_arr.append(sum)
 
   # print(f"#{t + 1} {ans}")
