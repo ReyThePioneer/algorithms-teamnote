c4fb6bf772481daea191742c017b13a51f382d5f
yh.hong <ghddpgml@gmail.com>
2024-05-16 23:01:32 +0900

    swea D2 #1926

--- 3-swea-d2/1926.py
+++ 3-swea-d2/1926.py
@@ -4,12 +4,15 @@ N = int(input())
 
 for i in range(1, N + 1):
   ans_str = str(i)
-  ans_str = ans_str.replace('3', '-')
-  ans_str = ans_str.replace('6', '-')
-  ans_str = ans_str.replace('9', '-')
+  clap = ''
 
-if '-' in ans_str:
-  for j in range(10):
-    ans_str = ans_str.replace(f"{j}", '')
-    print(ans_str, end= ' ')
-  print()
+  for j in ans_str:
+    if int(j) == 0:
+      continue
+    elif int(j) % 3 == 0:
+      clap += '-'
+
+  if len(clap) == 0:
+    print(i, end=' ')
+  else:
+    print(clap, end=' ')
