03a1aaa93e6945f6cd5fa13a4b5becf568d12333
yh.hong <ghddpgml@gmail.com>
2024-05-18 12:55:04 +0900

    swea D3 #1289

---
+++ 4-swea-d3/1289.py
@@ -0,0 +1,19 @@
+#1289 원재의 메모리 복구하기
+
+T = int(input())
+
+for tc in range(T):
+  mem = list(str(int(input())))
+
+  check = '1'
+  cnt = 0
+
+  for i in mem:
+    if i == check:
+      cnt += 1
+      if check == '1':
+        check = '0'
+      else:
+        check = '1'
+
+  print(f"#{tc + 1} {cnt}")
