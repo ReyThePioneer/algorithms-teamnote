48ee1f38b3f05609de264409b02ed1dd7ae0d870
yh.hong <ghddpgml@gmail.com>
2024-06-28 17:53:21 +0900

    implementation: #11659

---
+++ 2-implementation/11659.py
@@ -0,0 +1,19 @@
+#11659 구간 합 구하기 4
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+arr = list(map(int, sys.stdin.readline().split()))
+
+prefix_sum = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
+
+print(prefix_sum)
+
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    sum = prefix_sum[j] - prefix_sum[i - 1]
+    print(sum)
