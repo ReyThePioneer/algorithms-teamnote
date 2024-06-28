0b8fa41ed86aa95eb1a9d3b829604e71a4ed54df
yh.hong <ghddpgml@gmail.com>
2024-06-28 18:01:17 +0900

    EDIT directory names

--- 02-implementation/11659.py
+++ 02-implementation/11659.py
@@ -11,8 +11,6 @@ prefix_sum = [0] * (n + 1)
 for i in range(1, n + 1):
     prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
 
-print(prefix_sum)
-
 for _ in range(m):
     i, j = map(int, sys.stdin.readline().split())
     sum = prefix_sum[j] - prefix_sum[i - 1]
